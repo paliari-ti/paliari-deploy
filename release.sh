2789288c19826fb5b9f99c0ac5d377c2bb244df9
Marcos Paliari <marcos@paliari.com.br>
2018-12-04 09:10:36 -0200

    add coments

--- release.sh
+++ release.sh
@@ -10,29 +10,35 @@ echo '---------------------------'
 echo 'START SERVER RELEASE'
 echo
 
+# Validate APP path exists
 if [[ ! -d "$DIR" ]]; then
     echo "Path is required!"
     echo
     exit
 fi
 cd $DIR
+# Validate repo path exists in the APP
 if [[ ! -d "repo" ]]; then
   echo "Repo not found in '$DIR'"
   exit
 fi
-if [[ -f "repo/.deploy-hooks-repo" ]]; then
+
+# Exec hook in the repo
+if [[ -f "repo/.deploy/hooks-repo" ]]; then
   while read -r call; do
     echo "exec: [$call] in repo"
     cd repo/
     $call
-  done < "repo/.deploy-hooks-repo"
+  done < "repo/.deploy/hooks-repo"
 fi
 echo
 cd $DIR
 
+# Create release version
 mkdir -p releases/$version
 echo "Repo copy to releases/$version"
 cp -a repo/* releases/$version/
+# Link shared in release
 if [[ -d "shared" ]]; then
   for f in $DIR/shared/* $DIR/shared/.env; do
     ln -sf $f $DIR/releases/$version/
@@ -40,16 +46,18 @@ if [[ -d "shared" ]]; then
   echo "Created shared links in releases/$version/"
 fi
 
-if [[ -f "repo/.deploy-hooks-releases" ]]; then
+# Exec hooks in the release
+if [[ -f "repo/.deploy/hooks-releases" ]]; then
   while read -r call; do
     echo "exec: [$call] in releases/$version"
     cd releases/$version/
     $call
-  done < "repo/.deploy-hooks-releases"
+  done < "repo/.deploy/hooks-releases"
 fi
 echo
 cd $DIR
 
+# Link current in the release version
 echo "Replece current to releases/$version/"
 cp -a $DIR/releases/$version $DIR/current_tmp
 mv current current_old
