08f0af16b98c394ed2fe53bd4c04ea6b2e6434d4
Marcos Paliari <marcos@paliari.com.br>
2018-12-04 19:32:40 -0200

    comments

--- bin/gc-release.sh
+++ bin/gc-release.sh
@@ -7,18 +7,22 @@ MAX=${2:-10}
 
 echo
 echo "Garbage Collection in releases"
-echo
+
+# Check APP path exists
 if [[ ! "$DIR" || ! -d "$DIR" ]]; then
     echo "Path is required!"
     exit 1
 fi
 cd $DIR
+
+# Check releases path exists in the APP
 if [[ ! -d "releases" ]]; then
     echo "Releases path not found in '$DIR'"
     exit 1
 fi
 echo
 count=0
+# Loop in the releases order by time
 for f in $(ls -t releases/)
 do
   if [[ $count -gt $MAX ]]; then
