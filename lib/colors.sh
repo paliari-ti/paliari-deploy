56f05e519a4bcc85c187d501166b899a882f7b20
Marcos Paliari <marcos@paliari.com.br>
2018-12-05 11:31:34 -0200

    fix colors

--- lib/colors.sh
+++ lib/colors.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env sh
 
 echo_blue() {
-  printf "\033[34m$1\033[0m"
-  echo
+  printf "\033[34m$1\033[0m\n"
 }
 
 echo_green() {
-  printf "\033[32m$1\033[0m"
-  echo
+  printf "\033[32m$1\033[0m\n"
 }
 
 echo_yellow() {
-  printf "\033[33m$1\033[0m"
-  echo
+  printf "\033[33m$1\033[0m\n"
 }
 
 echo_red() {
-  printf "\033[31m$1\033[0m"
-  echo
+  printf "\033[31m$1\033[0m\n"
 }
