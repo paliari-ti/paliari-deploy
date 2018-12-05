f9774a9789d87dbae7a5c0607815be905e21aa82
Marcos Paliari <marcos@paliari.com.br>
2018-12-05 19:29:02 -0200

    fix install

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ rm -rf paliari-deploy
 git clone --depth 1 git@git.paliari.com.br:paliari/paliari-deploy.git
 cd paliari-deploy/
 rm -f /usr/local/bin/paliari-deploy
-ln ~/paliari-deploy/deploy.sh /usr/local/bin/paliari-deploy
+ln -s ~/paliari-deploy/deploy.sh /usr/local/bin/paliari-deploy
 echo
 echo "Success"
 echo
