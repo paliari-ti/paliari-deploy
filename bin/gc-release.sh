10435961869598124f148e0c66d66ff2ee1776d2
Marcos Paliari <marcos@paliari.com.br>
2018-12-05 15:18:17 -0200

    colored messages

--- bin/gc-release.sh
+++ bin/gc-release.sh
@@ -2,22 +2,25 @@
 
 set -e
 
+RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
+source ${RUN_DIR}/lib/colors.sh
+
 DIR=$1
 MAX=${2:-10}
 
 echo
-echo "Garbage Collection in releases"
+echo_yellow "Garbage Collection in releases"
 
 # Check APP path exists
 if [[ ! "$DIR" || ! -d "$DIR" ]]; then
-    echo "Path is required!"
+    echo_red "Path is required!"
     exit 1
 fi
 cd $DIR
 
 # Check releases path exists in the APP
 if [[ ! -d "releases" ]]; then
-    echo "Releases path not found in '$DIR'"
+    echo_red "Releases path not found in '$DIR'"
     exit 1
 fi
 echo
@@ -32,5 +35,5 @@ do
   count=$((count+1))
 done
 echo
-echo "Finished Garbage Collection"
+echo_green "Finished Garbage Collection"
 echo
