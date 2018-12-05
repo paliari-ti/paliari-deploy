c641290d6b1df20e76625531937083bef7a5677e
Marcos Paliari <marcos@paliari.com.br>
2018-12-05 19:10:20 -0200

    add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd ~/
+rm -rf paliari-deploy
+git clone --depth 1 git@git.paliari.com.br:paliari/paliari-deploy.git
+cd paliari-deploy/
+ln ~/paliari-deploy/deploy.sh /usr/local/bin/paliari-deploy
+echo
+echo "Success"
+echo
