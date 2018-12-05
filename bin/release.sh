482f5f75f83df7cf3d42a2543072bdc6653d9b7e
Marcos Paliari <marcos@paliari.com.br>
2018-12-05 10:41:25 -0200

    otimiza bin/release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -2,36 +2,41 @@
 
 set -e
 
-BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
+source ${RUN_DIR}/lib/yaml.sh
+source ${RUN_DIR}/lib/colors.sh
 
 DIR=$1
 version=${2:-$(date +%s)}
 
 echo
-echo '---------------------------'
-echo 'START SERVER RELEASE'
+echo_blue 'START SERVER RELEASE-----------------------'
 echo
 
 # Validate APP path exists
 if [[ ! -d "$DIR" ]]; then
-    echo "Path is required!"
-    echo
+    echo_red "APP path is required!"
     exit 1
 fi
 cd $DIR
 # Validate repo path exists in the APP
 if [[ ! -d "repo" ]]; then
-  echo "Repo not found in '$DIR'"
+  echo_red "Repo not found in '$DIR'"
   exit 1
 fi
 
+if [[ -f "$DIR/.deploy.config.yml" ]]; then
+  create_variables $DIR/.deploy.config.yml
+fi
+
 # Exec hook in the repo
-if [[ -f "repo/.deploy/hooks-repo" ]]; then
-  while read -r call; do
-    echo "exec: [$call] in repo"
-    cd repo/
+if [[ "${deploy_hooks_repo[*]}" ]]; then
+  cd repo/
+  for i in ${!deploy_hooks_repo[*]}; do
+    call=${deploy_hooks_repo[$i]}
+    echo_yellow "exec: [$call] in the repo"
     $call
-  done < "repo/.deploy/hooks-repo"
+  done
 fi
 echo
 cd $DIR
@@ -45,24 +50,25 @@ if [[ -d "shared" ]]; then
   for f in $DIR/shared/* $DIR/shared/.env; do
     ln -sf $f $DIR/releases/$version/
   done
-  echo "Created shared links in releases/$version/"
+  echo_green "Created shared links in releases/$version/"
 fi
+echo
 
 # Exec hooks in the release
-if [[ -f "repo/.deploy/hooks-releases" ]]; then
-  while read -r call; do
-    echo "exec: [$call] in releases/$version"
-    cd releases/$version/
+if [[ "${deploy_hooks_releases[*]}" ]]; then
+  cd releases/$version/
+  for i in ${!deploy_hooks_releases[*]}; do
+    call=${deploy_hooks_releases[$i]}
+    echo_yellow "exec: [$call] in the release"
     $call
-  done < "repo/.deploy/hooks-releases"
+  done
 fi
 echo
 cd $DIR
 
 # Link current in the release version
-$BIN_DIR/set-current-release.sh $DIR $version
+$RUN_DIR/bin/set-current-release.sh $DIR $version
 
 echo
-echo 'FINISHED SERVER RELEASE'
-echo '------------------------------'
+echo_blue 'FINISHED SERVER RELEASE--------------------'
 echo
